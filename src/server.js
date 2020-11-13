d6951f0c361759d3d9f8ccf47abf78b7f13cfb13
s1a2k1a3i <s.w.1213high@gmail.com>
2020-11-13 10:30:19 +0900

    public exec

--- src/server.js
+++ src/server.js
@@ -1,7 +1,11 @@
 const express = require("express");
+const path = require("path");
 
 const setupServer = () => {
   const app = express();
+
+  app.use(express.static(path.join(__dirname, "public")));
+
   app.use(express.json());
 
   app.get("/hello", (req, res) => {
